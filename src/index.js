54a23dd2c3fbbe927ca44b2d422483d807cc2c45
Enkeleda Elezi <Enkeleda.Elezi@iais.fraunhofer.de>
2019-06-14 15:16:22 +0200

    initial configuration of redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { createStore, applyMiddleware, compose } from "redux";
 import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
 import App from "./App";
+import reducers from "./reducers";
 import { BrowserRouter, Route } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
+
 ReactDOM.render(
   <BrowserRouter>
-    <Route path="/" component={App} />
+    <Provider store={store}>
+      <Route path="/" component={App} />
+    </Provider>
     {/* <App /> */}
   </BrowserRouter>,
   document.getElementById("root")
